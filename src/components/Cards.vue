b473b757d21541c0d32876ae90659cf67d090455
Luca <luca.biotti.flores@gmail.com>
2021-10-06 14:33:30 -0300

    layout hecho con vuetify

---
+++ src/components/Cards.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container class="grey background lighten-5">
+    <v-row no-glutters>
+      <v-col v-for="(personaje, index) in $store.state.personajes"
+            :key="index" cols="12" sm="4">
+        <v-card class="cardColor mx-auto mt-5 " max-width="400">
+          <v-img
+          
+            class="white--text align-end"
+            height="350px"
+            :src="personaje.imagen"
+          >
+            <v-card-title></v-card-title>
+          </v-img>
+          <v-card-subtitle class="pb-0 "></v-card-subtitle>
+          <v-card-text class="text--primary">
+            <div>{{ personaje.nombre }}</div>
+          </v-card-text>
+
+          <v-card-actions>
+         
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Cards'
+}
+</script>
+
+<style>
+  .cardColor {
+background: rgba( 126, 211, 33, 0.25 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 6.5px );
+-webkit-backdrop-filter: blur( 6.5px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mb-16">
+
+    <v-app-bar app >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <h3>Rick And Morty App</h3>
+    </v-app-bar>
+
+    <v-navigation-drawer  absolute app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Inicio</v-list-item-title>
+          <v-list-item-title>Buscar</v-list-item-title>
+          <v-list-item-title>Acerca De</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+     data: () => ({
+     drawer: false,
+  }),
+  name: 'Navbar',
+  links: ['Home', 'About Us', 'Team', 'Services', 'Blog', 'Contact Us']
+}
+</script>
+
+<style></style>
